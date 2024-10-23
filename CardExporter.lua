df2cf2523b202c62d1151214c66f70235e8f2690
elbe <elbenews@gmail.com>
2024-10-23 14:15:19 -0500

    add filter

--- CardExporter.lua
+++ CardExporter.lua
@@ -670,6 +670,7 @@ end
 
 local createOptionsRef = create_UIBox_options
 function create_UIBox_options()
+    G.EXPORT_FILTER = "" 
     local contents = createOptionsRef()
     if G.STAGE == G.STAGES.RUN then
         local m = UIBox_button({
@@ -718,7 +719,25 @@ function config_export_tab()
         },
         nodes = {
             UIBox_button({label = {"Export Cards"}, button = "create_output", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
-            UIBox_button({label = {"Test Shader"}, button = "test_shader", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
+            --UIBox_button({label = {"Test Shader"}, button = "test_shader", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
+            {
+                n = G.UIT.R,
+                config = { align = "cm" },
+                nodes = {
+                    create_text_input({
+                        colour = G.C.RED,
+                        hooked_colour = darken(copy_table(G.C.RED), 0.3),
+                        w = 4.5,
+                        h = 1,
+                        max_length = 100,
+                        extended_corpus = true,
+                        prompt_text = "Mod IDs, 'Balatro' for non-mod objects",
+                        ref_table = G,
+                        ref_value = "EXPORT_FILTER",
+                        keyboard_offset = 1,
+                    }),
+                },
+            }, --textbox
         },
     }
 end
@@ -757,7 +776,41 @@ G.FUNCS.test_shader = function(e)
    -- love.graphics.pop()
 end
 
+function table.contains(table, element)
+	for _, value in pairs(table) do
+		if value == element then
+			return true
+		end
+	end
+	return false
+end
+
+local function filterListFromString(str)
+    str = string.gsub(str, " ", "")
+    --str = string.lower(str)
+    str = string.split(str) --remove spaces from filter and separate via commas for compat with above functions
+    return str
+end
+
 G.FUNCS.create_output = function(e)
+    local mod_filter = filterListFromString(G.EXPORT_FILTER or "")
+    local clean_filter = {}
+    if #mod_filter == 0 then
+        table.insert(clean_filter, "Balatro")
+        for k,_ in pairs(SMODS.Mods) do
+            table.insert(clean_filter, k)
+        end
+    else
+        for _,v in ipairs(mod_filter) do
+            if v == "Balatro" then
+                table.insert(clean_filter, "Balatro")
+            end
+            if SMODS.Mods[v] then
+                table.insert(clean_filter, v)
+            end
+        end
+    end
+
     local card = nil
     if not love.filesystem.exists(output_root) then
         love.filesystem.createDirectory(output_root)
@@ -767,80 +820,34 @@ G.FUNCS.create_output = function(e)
     end
 
     for k,v in pairs(G.P_CENTERS) do
-        print("Processing " .. k .. " | " .. tostring(v.set))
-        v.unlocked = true
-        v.discovered = true
-        if v.set == "Edition" then
-            card = Card(G.jokers.T.x + G.jokers.T.w/2, G.jokers.T.y, G.CARD_W, G.CARD_H, G.P_CARDS.empty, v)
-            card:set_edition(v.key, true, true)
-            card:hover()
-            process_edition(card)
-        elseif v.set == "Enhanced" then
-            card = Card(G.jokers.T.x + G.jokers.T.w / 2, G.jokers.T.y, G.CARD_W, G.CARD_H, G.P_CARDS.empty, v)
-			card:set_ability(v, true, true)
-            card:hover()
-            process_enhancement(card)
-        elseif v.set == "Sticker" then
-            card = create_card("Default", G.jokers, nil, nil, nil, nil, "c_base", nil)
-            card:set_sticker(v, true, true)
-            card:hover()
-        elseif not v.set or v.set == "Other" or v.set == "Default" then
-        else
-            card = create_card(v.set, G.jokers, v.legendary, v.rarity, nil, nil, v.key, nil)
-            card:hover()
-            process_card(card)
+        if not v.mod then
+            v.mod = {}
+            v.mod.id = "Balatro"
         end
-        if card then
-            card:stop_hover()
-            G.jokers:remove_card(card)
-            card:remove()
-        end
-        card = nil
-    end
-
-    for k,v in pairs(G.P_BLINDS) do
-        print("Processing " .. k .. " | " .. tostring(v.set))
-        v.discovered = true
-        process_blind(v)
-    end
-
-    if G.P_CURSES then
-        for k,v in pairs(G.P_CURSES) do
+        if table.contains(clean_filter, v.mod.id) then
             print("Processing " .. k .. " | " .. tostring(v.set))
+            v.unlocked = true
             v.discovered = true
-            process_curse(v)
-        end
-    end
-
-    if G.P_D6_SIDES then
-        for k,v in pairs(G.P_D6_SIDES) do
-            print("Processing " .. k .. " | " .. tostring(v.set))
-            process_d6_side(v)
-        end
-    end
-
-    for k,v in pairs(G.P_SEALS) do
-        print("Processing " .. k .. " | " .. tostring(v.set))
-        v.discovered = true
-        card = create_card("Default", G.jokers, nil, nil, nil, nil, "c_base", nil)
-        card:set_seal(v.key, true)
-        card:hover()
-        process_seal(card, v)
-        if card then
-            card:stop_hover()
-            G.jokers:remove_card(card)
-            card:remove()
-        end
-        card = nil
-    end
-
-    if G.P_SKILLS then
-        for k,v in pairs(G.P_SKILLS) do
-            print("Processing " .. k .. " | " .. tostring(v.set))
-            v.discovered = true
-            card = Card(G.jokers.T.x + G.jokers.T.w/2, G.jokers.T.y, G.CARD_W, G.CARD_H, nil, v, { bypass_discovery_center = true})
-            card:hover()
-            process_card(card)
+            if v.set == "Edition" then
+                card = Card(G.jokers.T.x + G.jokers.T.w/2, G.jokers.T.y, G.CARD_W, G.CARD_H, G.P_CARDS.empty, v)
+                card:set_edition(v.key, true, true)
+                card:hover()
+                process_edition(card)
+            elseif v.set == "Enhanced" then
+                card = Card(G.jokers.T.x + G.jokers.T.w / 2, G.jokers.T.y, G.CARD_W, G.CARD_H, G.P_CARDS.empty, v)
+                card:set_ability(v, true, true)
+                card:hover()
+                process_enhancement(card)
+            elseif v.set == "Sticker" then
+                card = create_card("Default", G.jokers, nil, nil, nil, nil, "c_base", nil)
+                card:set_sticker(v, true, true)
+                card:hover()
+            elseif not v.set or v.set == "Other" or v.set == "Default" then
+            else
+                card = create_card(v.set, G.jokers, v.legendary, v.rarity, nil, nil, v.key, nil)
+                card:hover()
+                process_card(card)
+            end
             if card then
                 card:stop_hover()
                 G.jokers:remove_card(card)
@@ -850,50 +857,160 @@ G.FUNCS.create_output = function(e)
         end
     end
 
+    for k,v in pairs(G.P_BLINDS) do
+        if not v.mod then
+            v.mod = {}
+            v.mod.id = "Balatro"
+        end
+        if table.contains(clean_filter, v.mod.id) then
+            print("Processing " .. k .. " | " .. tostring(v.set))
+            v.discovered = true
+            process_blind(v)
+        end
+    end
+
+    if G.P_CURSES then
+        for k,v in pairs(G.P_CURSES) do
+            if not v.mod then
+                v.mod = {}
+                v.mod.id = "Balatro"
+            end
+            if table.contains(clean_filter, v.mod.id) then
+                print("Processing " .. k .. " | " .. tostring(v.set))
+                v.discovered = true
+                process_curse(v)
+            end
+        end
+    end
+
+    if G.P_D6_SIDES then
+        for k,v in pairs(G.P_D6_SIDES) do
+            if not v.mod then
+                v.mod = {}
+                v.mod.id = "Balatro"
+            end
+            if table.contains(clean_filter, v.mod.id) then
+                print("Processing " .. k .. " | " .. tostring(v.set))
+                process_d6_side(v)
+            end
+        end
+    end
+
+    for k,v in pairs(G.P_SEALS) do
+        if not v.mod then
+            v.mod = {}
+            v.mod.id = "Balatro"
+        end
+        if table.contains(clean_filter, v.mod.id) then
+            print("Processing " .. k .. " | " .. tostring(v.set))
+            v.discovered = true
+            card = create_card("Default", G.jokers, nil, nil, nil, nil, "c_base", nil)
+            card:set_seal(v.key, true)
+            card:hover()
+            process_seal(card, v)
+            if card then
+                card:stop_hover()
+                G.jokers:remove_card(card)
+                card:remove()
+            end
+            card = nil
+        end
+    end
+
+    if G.P_SKILLS then
+        for k,v in pairs(G.P_SKILLS) do
+            if not v.mod then
+                v.mod = {}
+                v.mod.id = "Balatro"
+            end
+            if table.contains(clean_filter, v.mod.id) then
+                print("Processing " .. k .. " | " .. tostring(v.set))
+                v.discovered = true
+                card = Card(G.jokers.T.x + G.jokers.T.w/2, G.jokers.T.y, G.CARD_W, G.CARD_H, nil, v, { bypass_discovery_center = true})
+                card:hover()
+                process_card(card)
+                if card then
+                    card:stop_hover()
+                    G.jokers:remove_card(card)
+                    card:remove()
+                end
+                card = nil
+            end
+        end
+    end
+
     for k,v in pairs(G.P_STAKES) do
-        print("Processing " .. k .. " | " .. tostring(v.set))
-        v.discovered = true
-        process_stake(v)
+        if not v.mod then
+            v.mod = {}
+            v.mod.id = "Balatro"
+        end
+        if table.contains(clean_filter, v.mod.id) then
+            print("Processing " .. k .. " | " .. tostring(v.set))
+            v.discovered = true
+            process_stake(v)
+        end
     end
 
     for k,v in pairs(SMODS.Stickers) do
-        --print("Processing " .. k .. " | " .. tostring(v.set))
-        --v.discovered = true
-        --process_sticker(v)
+        --if table.contains(clean_filter, v.mod.id) then
+            --print("Processing " .. k .. " | " .. tostring(v.set))
+            --v.discovered = true
+            --process_sticker(v)
+        --end
     end
 
     for k,v in pairs(G.P_TAGS) do
-        print("Processing " .. k .. " | " .. tostring(v.set))
-        v.discovered = true
-        local temp_tag = Tag(v.key, true)
-        local _, temp_tag_sprite = temp_tag:generate_UI()
-        temp_tag_sprite:hover()
-        process_tag(temp_tag)
-        temp_tag_sprite:stop_hover()
-        temp_tag_sprite:remove()
-        temp_tag = nil
+        if not v.mod then
+            v.mod = {}
+            v.mod.id = "Balatro"
+        end
+        if table.contains(clean_filter, v.mod.id) then
+            print("Processing " .. k .. " | " .. tostring(v.set))
+            v.discovered = true
+            local temp_tag = Tag(v.key, true)
+            local _, temp_tag_sprite = temp_tag:generate_UI()
+            temp_tag_sprite:hover()
+            process_tag(temp_tag)
+            temp_tag_sprite:stop_hover()
+            temp_tag_sprite:remove()
+            temp_tag = nil
+        end
     end
 
     for k,v in pairs(G.P_CARDS) do
-        print("Processing " .. k .. " | " .. tostring(v.suit))
-        card = create_playing_card({front = G.P_CARDS[k]},  G.hand, true, true, {G.C.SECONDARY_SET.Spectral})
-        card:hover()
-        process_playing_card(card, v, k)
-        if card then
-            card:stop_hover()
-            G.jokers:remove_card(card)
-            card:remove()
+        if not v.mod then
+            v.mod = {}
+            v.mod.id = "Balatro"
+        end
+        if table.contains(clean_filter, v.mod.id) then
+            print("Processing " .. k .. " | " .. tostring(v.suit))
+            card = create_playing_card({front = G.P_CARDS[k]},  G.hand, true, true, {G.C.SECONDARY_SET.Spectral})
+            card:hover()
+            process_playing_card(card, v, k)
+            if card then
+                card:stop_hover()
+                G.jokers:remove_card(card)
+                card:remove()
+            end
         end
     end
 
     for k,v in pairs(SMODS.Suits) do
-        print("Processing " .. k .. " | " .. tostring(v.key))
-        process_suit(v)
+        if not v.mod then
+            v.mod = {}
+            v.mod.id = "Balatro"
+        end
+        if table.contains(clean_filter, v.mod.id) then
+            print("Processing " .. k .. " | " .. tostring(v.key))
+            process_suit(v)
+        end
     end
 
     for k,v in pairs(SMODS.Mods) do
-        print("Processing " .. k .. " | " .. tostring(v.name))
-        process_mod(v)
+        if table.contains(clean_filter, k) then
+            print("Processing " .. k .. " | " .. tostring(v.name))
+            process_mod(v)
+        end
     end
 
     local output = json.encode(sets)
